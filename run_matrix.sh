f4473cc57ed23dca757351dc7e9693662101f243
Anthony Kourmoulakis <antmanofgod@gmail.com>
2026-01-27 21:56:27 -0500

    Fix syntax for running matrix_animation.py

--- run_matrix.sh
+++ run_matrix.sh
@@ -2,5 +2,5 @@
 
 # Run matrix_animation.py inside Konsole and exit AFTER it starts
 konsole --hold -e bash -c "
-    python3 \"$(dirname "$0")/matrix_animation.py\"
+    python3 "$(dirname "$0")/matrix_animation.py"
 "
